6e5d4dbd7a5e27ac6f21a5cdb0b10c56ded6a0c2
mtremoureux <matheo.tremoureux@etudiant.univ-rennes.fr>
2024-04-13 12:15:33 +0200

    fix

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -1,23 +1,30 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
-import { CheckBox } from 'react-native-elements';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+} from "react-native";
+import { CheckBox } from "react-native-elements";
 
 const LoginScreen = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [isChecked, setIsChecked] = useState(false);
 
   const handleLogin = () => {
     // Replace with your actual authentication logic
     // Here's a simple example for demonstration purposes
-    if (username === 'admin' && password === 'password') {
+    if (username === "admin" && password === "password") {
       // Login successful
-      Alert.alert('Success!', 'Login successful');
+      Alert.alert("Success!", "Login successful");
       // You can navigate to another screen here, for example:
       // navigation.navigate('Dashboard');
     } else {
       // Login failed
-      Alert.alert('Error', 'Invalid username or password');
+      Alert.alert("Error", "Invalid username or password");
     }
   };
 
@@ -37,7 +44,7 @@ const LoginScreen = () => {
         value={password}
         onChangeText={setPassword}
       />
-{/*       <View style={styles.checkboxContainer}>
+      {/*       <View style={styles.checkboxContainer}>
         <CheckBox
           checked={isChecked}
           onPress={() => setIsChecked(!isChecked)}
@@ -54,39 +61,39 @@ const LoginScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 20,
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
   },
   input: {
-    width: '100%',
+    width: "100%",
     padding: 15,
     marginBottom: 20,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 5,
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
   },
   checkboxContainer: {
     marginBottom: 20,
   },
   button: {
-    width: '100%',
+    width: "100%",
     padding: 15,
-    backgroundColor: '#8572FF',
+    backgroundColor: "#8572FF",
     borderRadius: 5,
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 20,
   },
   buttonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 });
 
